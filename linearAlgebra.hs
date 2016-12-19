9e9d451adbdddc6c986aa7d91bd1b5ec06b67b95
nickyli98 <nickyli98@gmail.com>
2016-12-19 07:52:37 +0000

    matrix product works

--- linearAlgebra.hs
+++ linearAlgebra.hs
@@ -1,17 +1,25 @@
 -- Pre: the two matrix are multipliable
-{-matrixProduct :: [[Int]] -> [[Int]] -> [[Int]]
+matrixProduct :: [[Int]] -> [[Int]] -> [[Int]]
 matrixProduct a b
-   = [rowOutput ]
+   = [rowOutput x (transpose b) | x <- a]
     where
-      k = length a
-      n = length b
-      m = length (head b)-}
+      rowOutput :: [Int] -> [[Int]] -> [Int]
+      rowOutput _ [] = []
+      rowOutput a (x:xs)
+         = sumOfProducts a x : rowOutput a xs
 
+--Takes two lists of ints and sums the products in order
+--Pre the two lists are of same length
+sumOfProducts :: [Int] -> [Int] -> Int
+sumOfProducts a b
+   = sum (zipWith (*) a b)
+
+--Tranposes a matrix
 transpose :: [[Int]] -> [[Int]]
 transpose a
-   = tranpose' a (length (head a)) 0
-
-transpose' :: [[Int]] -> Int -> Int -> [[Int]]
-tranpose' a x y
-   | x == y = []
-   | otherwise = [b !! y | b <- a] : tranpose a x (y + 1)
+   = transpose' a (length (head a)) 0
+    where
+      transpose' :: [[Int]] -> Int -> Int -> [[Int]]
+      transpose' a x y
+       | x == y = []
+       | otherwise = [b !! y | b <- a] : transpose' a x (y + 1)
