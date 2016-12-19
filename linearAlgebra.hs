9f7ed4f63949730e1ca325f6bb4e172eac4fbb5a
nickyli98 <nickyli98@gmail.com>
2016-12-19 17:26:50 +0000

    replaced all int with float

--- linearAlgebra.hs
+++ linearAlgebra.hs
@@ -1,50 +1,60 @@
 -- Pre: the two matrix are multipliable
-matrixProduct :: [[Int]] -> [[Int]] -> [[Int]]
+matrixProduct :: [[Float]] -> [[Float]] -> [[Float]]
 matrixProduct a b
    = [rowOutput x (transpose b) | x <- a]
     where
-      rowOutput :: [Int] -> [[Int]] -> [Int]
+      rowOutput :: [Float] -> [[Float]] -> [Float]
       rowOutput _ [] = []
       rowOutput a (x:xs)
          = sumOfProducts a x : rowOutput a xs
 
---Takes two lists of ints and sums the products in order
+--Takes two lists of Floats and sums the products in order
 --Pre the two lists are of same length
-sumOfProducts :: [Int] -> [Int] -> Int
+sumOfProducts :: [Float] -> [Float] -> Float
 sumOfProducts a b
    = sum (zipWith (*) a b)
 
 --Tranposes a matrix
-transpose :: [[Int]] -> [[Int]]
+transpose :: [[Float]] -> [[Float]]
 transpose a
    = transpose' a (length (head a)) 0
     where
-      transpose' :: [[Int]] -> Int -> Int -> [[Int]]
+      transpose' :: [[Float]] -> Int -> Int -> [[Float]]
       transpose' a x y
        | x == y = []
        | otherwise = [b !! y | b <- a] : transpose' a x (y + 1)
-
+{-
 -- Converts a matrix to RREF
-rref :: [[Int]] -> [[Int]]
+rref :: [[Float]] -> [[Float]]
 rref a = rref' a b
    where
      b = [findConsecutiveZeros x | x <- a]
-     rref' :: [[Int]] -> [Int] -> [[Int]]
+     rref' :: [[Float]] -> [Float] -> [[Float]]
      rref' a b
        | ordered b = rowReduce a b
        | otherwise = order a b
-
+-}
+--checks if a list os ordered
+ordered :: [Float] -> Bool
+ordered [] = True
+ordered [x] = True
+ordered (x:y:xs)
+   | x <= y = ordered (y:xs)
+   | otherwise = False
+{-
 -- Swaps rows in a Matrix by using the number of zeros
-order :: [[Int]] -> [Int] -> [[Int]]
+order :: [[Float]] -> [Float] -> [[Float]]
+
 
 -- Row A - Row B of multiple x
-rowReduce :: Float -> [Int] -> [Int] -> [Int]
+rowReduce :: Float -> [Float] -> [Float] -> [Float]
 rowReduce x a b
-
-findConsecutiveZeros :: [Int] -> Int
+-}
+-- Finds the number of consecutive zeros in the beginning on a list of numbers
+findConsecutiveZeros :: [Float] -> Float
 findConsecutiveZeros a = findConsecutiveZeros' a 0
    where
-     findConsecutiveZeros' :: [Int] -> Int -> Int
+     findConsecutiveZeros' :: [Float] -> Float -> Float
      findConsecutiveZeros' [] x = x
      findConsecutiveZeros' (x:xs) y
         | x == 0 = findConsecutiveZeros' xs (y + 1)
